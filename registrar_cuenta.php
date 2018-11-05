6448fad3414a02e3db2f56ec4a7af9834e3fce39
YenySayda <44480771+YenySayda@users.noreply.github.com>
2018-11-05 18:22:23 -0500

    Create registrar_cuenta.php

---
+++ registrar_cuenta.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JONATHAN'S EL TURISTA</title>
+    <link rel="shortcut icon" href="https://media-cdn.tripadvisor.com/media/photo-s/05/3f/e6/99/peru-trek-4-good-day.jpg" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>JONATHAN'S EL TURISTA</h1>
+
+    <?php include 'cabecera.php' ?>
+
+    <h2>formulario paea crear  su ceinta en JONATHAN'S EL TURISTA</h2>
+
+        <p>¡BIENVENID@!</p>
+        <p>UD. ah sido Registrado</p>
+
+</body>
+</html>

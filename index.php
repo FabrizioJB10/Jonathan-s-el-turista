ef521154fceb1aebcaadaf707824a47480fd290d
YenySayda <44480771+YenySayda@users.noreply.github.com>
2018-11-05 18:00:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>JONATHAN´S EL TURISTA</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <title>JONATHAN'S EL TURISTA</title>
+    <link rel="shortcut icon" href="https://media-cdn.tripadvisor.com/media/photo-s/05/3f/e6/99/peru-trek-4-good-day.jpg" />
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <?php include 'cabecera.php' ?>
+    <h1 style="color: blue;">BIEVENID@ a: JONATHAN'S EL TURISTA!</h1>
+    <h4>Conoce los mejores lugares del Perú. <br>
+    ¡Con JONATHAN’s el turista, podrás viajar y compartir tu experiencia con el mundo!</h4>    
+    <?php include 'footer.php' ?>
+
 </body>
 </html>

388f8f942fe7c2ee33a06b4d0eb104050df17122
YenySayda <44480771+YenySayda@users.noreply.github.com>
2018-11-05 18:03:19 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <img src="https://media-cdn.tripadvisor.com/media/photo-s/05/3f/e6/99/peru-trek-4-good-day.jpg" alt="" height="100">
+    <img src="https://enviajes.cl/wp-content/uploads/2013/10/Lugares-turisticos-de-Peru-Valle-Incas.jpg" alt="" height="100">
+    <img src="https://enviajes.cl/wp-content/uploads/2013/10/Peru-Turismo-Titicaca.jpg" alt="" height="100">
+    <img src="https://enviajes.cl/wp-content/uploads/2013/10/Peru-Turismo-Chiclayo.jpg" alt="" height="100">
+    <img src="https://enviajes.cl/wp-content/uploads/2013/10/Peru-Turismo-Iquitos.jpg" alt="" height="100">
+    <img src="http://adventuretravelperu.com/es/wp-content/uploads/2015/12/valle_de-colca_arequipa_turismo_peru.jpg" alt="" height="100">
+    <img src="http://2.bp.blogspot.com/-VwjcHDOvLxE/UfBgrTIEgVI/AAAAAAAAAJA/1hJB2lEqCXA/s1600/los-mejores-destinos-tur%C3%ADsticos-del-per%C3%BA-tepsa4.jpg" alt="" height="100">
+    <img src="https://peru.travelguia.net/files/2009/08/bosuqe-de-piedras-de-sachapite1.jpg" alt="" height="100">
+
+    <p style="color: green;"><strong><em>JONATHAN'S EL TURISTA-CONTIGO 2018</p></em></strong>
+</footer>

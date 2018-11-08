c17e221173f1d399676cd11554386e958035edc0
YenySayda <44480771+YenySayda@users.noreply.github.com>
2018-11-07 20:05:13 -0500

    Update lugares_turisticos.php

--- lugares_turisticos.php
+++ lugares_turisticos.php
@@ -11,11 +11,79 @@
 <body>
     <?php include 'cabecera.php' ?>
     <h1>**LUGARES TURÍSTICOS **</h1>
-    <div>
-        <img src="" alt="">    
-    
+    <div class="caja">
+        <h3>1.MACHU PICCHU</h3>
+        <img src="https://enviajes.cl/wp-content/uploads/2013/10/Lugares-turisticos-de-Peru-Machupichu.jpg" alt="MACHU PICCHU">    
+        <P>Impresionante ciudad incaica levantada por sobre
+         los 2.360 metros sobre el nivel del mar, sobrecogedor paisaje de murallas, 
+         terrazas y rampas que parecieran esculpidas en la roca misma, inmersas entre majestuosos picos de montañas, acantilados y el verdor de la floresta amazónica.</P>
     </div>
-
+    <div class="caja">
+        <h3>2. valle sagrado d elos incas</h3>
+        <p>Mágica área de quebradas, sitios arqueológicos y 
+        pintorescos pueblitos coloniales, que se suceden uno 
+        tras otros, habitados por comunidades indígenas, que han 
+        mantenido vivo en el tiempo su folclore y tradiciones.</p>
+    </div>
+    <div class="caja">
+        <h3>3. AREQUIPA-Plaza de Armas de Arequipa</h3>
+        <p>Apodada como la Ciudad Blanca, una ciudad que 
+        encanta por su apariencia colonial, con un despliegue
+         de arquitecturas en piedra volcánica blanca y un 
+         hermoso centro histórico declarado Patrimonio de la
+          Humanidad por la UNESCO.</p>
+    </div>
+    <div class="caja">
+        <h3>4. LAGO TITICACA-Islas flotantes de los Uros</h3>
+        <p>Ubicado a 3.812 metros sobre el nivel del mar, corresponde 
+        al lago navegable más alto del mundo, una inmensa masa de agua
+         escondida entre los paisajes montañosos de la Cordillera de los
+          Andes, en la frontera de Perú y Bolivia, cercano a la ciudad Puno.Su mayor atractivo 
+          lo constituyen sus islas, hogar de diversas
+        comunidades indígenas que mantienen vivas sus tradiciones ancestrales.
+     Las más famosas entre ellas, corresponden a Taquile, hogar de los quechuas, 
+    famosos por sus vestimentas y arte textil, Amantaní con vestigios arqueológicos
+     dejados por la cultura Tiahuanaco y las islas flotantes de los Uros,
+    correspondientes a un grupo de islas artificiales elaboradas en totora 
+    por la etnia Uru, quienes habitan en ellas sobreviviendo de la pesca, 
+    caza y elaboración de tejidos.</p>
+    </div><div class="caja">
+        <h3>5.MANCORA-Playas de Mancora</h3>
+        <p>Sin duda uno de los mejores lugares turísticos de 
+        Perú para disfrutar del descanso, entre kilómetros de 
+        playas de arenas blancas y aguas cálidas rodeadas de 
+        frondosa vegetación, olas excepcionales para la práctica 
+        de surf y un clima privilegiado, con temperaturas de verano prácticamente todo el año.
+El pueblo más bien pequeño, también tiene su interés, una extensa avenida principal, atiborrada de tiendas de artesanías y locales donde alquilar tablas de surf; y al menos una decena de bares que encienden las noches con ritmos reggae</p>
+    </div>
+    <div class="caja">
+        <h3>6.CHICLAYO-Entierro real en el Señor del Sipán (Gobernador, esposa, concubinas y guardias)</h3>
+        <img src="" alt="CHICLAYO-Entierro real en el Señor del Sipán (Gobernador, esposa, concubinas y guardias)">
+        <p>Conocida como la ciudad de los brujos, debido a la intensa atmósfera esotérica presente, siendo hogar de un gran número de chamanes, curanderos y adivinos, quienes normalmente atienden a personas que acuden de distintos rincones del país a buscar respuesta a sus males. Entre sus sitios más famosos, destaca el Mercado de los Brujos, un curioso mercado que se instala cerca de la plaza de armas, donde es posible adquirir todo tipo de ingredientes para la elaboración de pócimas y medicinas alternativas.
+Chiclayo, también es una ciudad famosa, por sus entornos arqueológicos, entre los que figuran sitios como el Entierro Real del Señor de Sipán y las Pirámides de Túcume, impresionantes enclaves de las culturas Mochica y Lambayeque, civilizaciones que alcanzaron su esplendor antes del Imperio Inca.</p>
+    </div>
+    <div class="caja">
+        <h3>7.IQUITOS-Playas de Iquitos</h3>
+        <img src="https://enviajes.cl/wp-content/uploads/2013/10/Peru-Turismo-Iquitos.jpg" alt="IQUITOS-Playas de Iquitos">
+        <p>Puerta de entrada a la extraordinaria belleza de la selva amazónica, el bosque tropical más extenso del planeta, declarado el 2011 como una de las siete maravillas naturales del mundo. Desde la ciudad se inician la mayoría de las rutas de exploración que permiten internarse por sus bosques vírgenes, ya sea navegando por el caudaloso Río Amazonas o a través de caminatas por sus impresionantes reservas.
+Entre éstas, destaca la enorme Reserva Nacional Pacaya Samiria, conocida como la “Selva de los Espejos”, con un tamaño que podría ser comparado al 50% de la superficie que ocupa Suiza o Dinamarca, albergando enormes pantanales y lagunas, donde es posible observar caimanes, delfines rosados y boas anacondas, entre otras 1.025 especies animales.</p>
+    </div>
+    <div class="caja">
+        <h3>8.Líneas de Nazca</h3>
+        <img src="https://enviajes.cl/wp-content/uploads/2013/10/Peru-Turismo-Nazca.jpg" alt="Líneas de Nazca">
+        <p>Enigmático enclave arqueológico ubicado en las Pampas de Jumana, donde enormes geoglifos que ocupan superficies kilométricas, se extienden entre los cerros y la superficie del desierto, adoptando formas animales, vegetales y diseños geométricos.
+Declaradas Patrimonio de la Humanidad por la UNESCO, corresponden probablemente a uno de los lugares turísticos de Perú y posiblemente de América, que han generado mayor curiosidad y misticismo relativas a su autoría y finalidad. Si bien hoy se sabe que fueron trazadas por la cultura Nazca 200 años A.C., su razón de ser aún continúa en estudio, predominando las teorías de que habrían sido realizadas como un gigante calendario astronómico, que indicaría los diversos movimientos del sol, la luna y las constelaciones.</p>
+    </div>
+    <div class="caja">
+        <h3>9. CUZCO-Centro histórico de Cuzco</h3>
+        <img src="https://enviajes.cl/wp-content/uploads/2013/10/Lugares-turisticos-de-Peru-Cuzco.jpg" alt="9. CUZCO-Centro histórico de Cuzco">
+        <p>Uno de los más fascinantes lugares turísticos de Perú, hogar de invaluables tesoros históricos y culturales, ruinas que aún permiten visualizar el esplendor de lo que fue la capital del Imperio Inca, y magníficos edificios civiles y religiosos que dan muestra de la riqueza de su época colonial.</p>
+    </div>
+    <div class="caja">
+        <h3>10. LIMA-Plaza de Armas de Lima</h3>
+        <img src="https://enviajes.cl/wp-content/uploads/2013/10/Lugares-turisticos-de-Peru-Lima.jpg" alt="LIMA-Plaza de Armas de Lima">
+        <p>Capital de Perú, reconocida en el 2013 con el premio Travellers’ Choice como mejor destino emergente, una ciudad que cautiva con su mezcla de modernidad e historia, en donde barrios de vibrante actividad y vida nocturna como Miraflores y San Isidro, se cruzan con las antiguas arquitecturas coloniales del Centro Histórico declarado Patrimonio de la Humanidad por la UNESCO.</p>
+    </div>
 
 
 

ea94ba5707705bf426a42d4ac976c75a89425449
FabrizioJB10 <44480722+FabrizioJB10@users.noreply.github.com>
2018-11-08 16:21:07 -0500

    Update lugares_frecuencia_turismo.php

--- lugares_frecuencia_turismo.php
+++ lugares_frecuencia_turismo.php
@@ -11,12 +11,86 @@
 <body>
     <?php include 'cabecera.php' ?>
     <h1>**LUGARES CON MÁS TURISMO**</h1>
-    
-
-
-    
-
-
+    <div class="caja">
+        <h3>1.RESERVA DE TAMBOPATA</h3>
+        <img src="https://cbctupay.com/wp-content/uploads/Tambopata.jpg" alt="TAMBOPATA" height="270">    
+        <P>Se dice que los bosques amazónicos son el único lugar que dan la sensación de presenciar el amanecer
+         de los tiempos. Miles de visitantes observan la diversidad de bosques y ríos del área al llegar a la
+          ciudad de Puerto Maldonado, la capital del departamento de Madre de Dios, listos para embarcarse en un 
+          viaje lleno de emoción y descubrimiento..</P>
+    </div>
+    <div class="caja">
+        <h3>2.Huaraz</h3>
+        <img src="http://www.lcperu.pe/images/Img-Promo/2018091748_max.png" alt="HUARAZ" height="270"> 
+        <P>Los orígenes de la ciudad se remontan a la época pre inca, con el desarrollo de los primeros asentamientos
+         humanos alrededor de los ríos Quilcay y Santa; su fundación española se llevó a cabo en 1574 como reducción
+          de indios.</P>
+    </div>
+    <div class="caja">
+        <h3>3.TUMBES</h3>
+        <img src="http://viajeronline.com/wp-content/uploads/como-viajar-y-llegar-a-la-ciudad-de-tumbes-desde-lima-1.jpg" alt="TUMBES" height="270"> 
+        <P>Tumbes es una ciudad del extremo noroeste del Perú, capital provincial y del Departamento de Tumbes,
+         situada cerca de la desembocadura del río Tumbes en el golfo de Guayaquil (océano Pacífico),
+          a 30 km de la frontera con Ecuador. Cuenta con una población estimada de 111 595 habitantes para
+           el año 2015</P>
+    </div>
+    <div class="caja">
+        <h3>4.CAMANA</h3>
+        <img src="https://turismoi.pe/uploads/city/image/20737/large_Camana_city.png" alt="CAMANA" height="270"> 
+        <P>Es el lugar donde los españoles fundaron originalmente la “Villa Hermosa” el 9 de noviembre de 1539,
+         trasladándose luego al valle de Arequipa en 1540. La ciudad de Arequipa tuvo su primera cuna en este 
+         riquísimo y extenso valle de Camaná. Luego fue fundada por segunda vez el 29 de setiembre de 
+         1557 como "Villa de San Miguel de Ribera".</P>
+    </div>
+    <div class="caja">
+        <h3>5.RESERVA NACIONAL EL MANU</h3>
+        <img src="https://www.elpopular.pe/sites/default/files/styles/img_620x465/public/imagen/2017/08/21/Noticia-187808-1472856779parque-nacional-del-manu.jpg?itok=3zanHYkB" alt="RESERVA NACONAL DEL MANU" height="270"> 
+        <P>Se extiende desde los 300 msnm, en la confluencia del río Manu con el río Madre de Dios, hasta los 
+        3800 msnm en la cumbre de la montaña Apu Kañajhuay. Algunos investigadores creen que en las zonas 
+        vírgenes de esta reserva se halla el Paititi o ciudad perdida de los Incas.
+        El parque nacional fue creado el 29 de mayo de 1973. En 1977, la UNESCO reconoce al Parque como Reserva 
+        de Biósfera y en 1987 lo declara Patrimonio Natural de la Humanidad</P>
+    </div>
+    <div class="caja">
+        <h3>6.CATEDRAL DE TRUJILLO</h3>
+        <img src="https://i1.wp.com/detrujillo.com/wp-content/uploads/2012/03/iglesia-catedral-de-trujillo-com-peru.png?fit=700%2C380&ssl=1" alt="CATEDRAL DE TRUJILLO" height="270"> 
+        <P>El Centro Histórico de la ciudad, conocido como «Cercado de Trujillo» se extiende sobre un área de 133.5
+         ha. constituye el núcleo urbano más importante y característico de la urbe, en él se encuentra gran parte 
+         de su patrimonio cultural, arquitectónico y urbanístico; asimismo concentra las principales actividades 
+         económicas, político-administrativas y socio–culturales de la ciudad.</P>
+    </div>
+    <div class="caja">
+        <h3>7.IGLESIA DE TARMA</h3>
+        <img src="https://img.elcomercio.pe/files/ec_article_multimedia_gallery/uploads/2017/06/06/593729a049587.jpeg" alt="IGLESIA DE TARMA" height="270"> 
+        <P>El asentamiento principal en la época de los incas era Tarmatambo. Con la implementación de las 
+        reducciones de indios bajo el virrey Francisco de Toledo (1569-1572), se trasladó la capital al fondo 
+        del valle, recibiendo el nombre de Santa Ana de Pampas.</P>
+    </div>
+    <div class="caja">
+        <h3>8.MOQUEGUA</h3>
+        <img src="https://prensaregional.pe/wp-content/uploads/2018/08/Moquegua-plaza-referencial.jpg" alt="MOQUEGUA" height="270"> 
+        <P>Moquegua es uno de los veinticuatro departamentos que forman la República del Perú.
+         Su capital es la homónima ciudad Moquegua.
+        Está ubicado al sur del país, en la vertiente occidental de la cordillera de los Andes. Limita al norte con 
+        Arequipa, al este con Puno, al sur con Tacna y al oeste con el océano Pacífico. Con 15 734 km² es el tercer 
+        departamento menos extenso —por delante de Lambayeque y Tumbes, el menos extenso— y con 10,3 hab/km² es el 
+        quinto menos densamente poblado —por delante de Amazonas, Ucayali, Loreto y Madre de Dios, el menos densamente 
+        poblado— Se fundó el dos de enero de 1837..</P>
+    </div>
+    <div class="caja">
+        <h3>9.PLAYA COLORADO-HUACHO</h3>
+        <img src="http://www.somosperu.org.pe/wp-content/uploads/2016/10/Playa-Colorado-Huacho.jpg" alt="PLAYA COLORADO-HUACHO" height="270"> 
+        <P>Huacho es una ciudad en la costa central del Perú, capital de la provincia de Huaura y sede regional 
+        del departamento de Lima, ubicada en una bahía formada por el océano Pacífico a 148 km al 
+        norte de la ciudad de Lima, próxima a la desembocadura del río Huaura.</P>
+    </div>
+    <div class="caja">
+        <h3>10.PIEDRA DE HUAMANGA-AYACUCHO</h3>
+        <img src="https://img.elcomercio.pe/files/ec_article_multimedia_gallery/uploads/2018/08/17/5b772e836dd25.jpeg" alt="PIEDRA DE HUAMANGA-AYACUCHO" height="270"> 
+        <P>Es uno de los conjuntos arquitectónicos y artísticos más notables del Perú. Se le conoce como la 
+        «Ciudad de las Iglesias»,2​ por sus numerosos templos coloniales, y como «Ciudad Señorial» 
+        por su arquitectura, tradición y arte..</P>
+    </div>
 
     <?php include 'footer.php' ?>
 

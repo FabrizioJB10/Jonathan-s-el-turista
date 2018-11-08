bbf976dc4fe6e45147feb5767b05e9bdd485f4b4
YenySayda <44480771+YenySayda@users.noreply.github.com>
2018-11-08 16:12:31 -0500

    Update lugares_exoticos.php

--- lugares_exoticos.php
+++ lugares_exoticos.php
@@ -12,7 +12,56 @@
     <?php include 'cabecera.php' ?>
 
     <h1>**LUGARES EXÓTICOS**</h1> 
-
+    <div class="caja">
+        <h3>1.Fortaleza de Sacsayhuamán en Cusco</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcK0QH.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="270">    
+        <P>Sacsayhuamán (en quechua Saqsaywaman, de saqsay, lleno o satisfecho, y waman, halcón, es decir, "halcón satisfecho") es una "fortaleza ceremonial" inca ubicada dos kilómetros al norte de la ciudad de Cusco. Se comenzó a construir durante el gobierno de Pachacútec, en el siglo XV; sin embargo, fue Huayna Cápac quien la culminó en el siglo XVI.</P>
+    </div>
+    <div class="caja">
+        <h3>2. Andenería en Moray, Cusco</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcJK2p.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f"height="270">
+        <p>Moray es unsitio arqueológico que se encuentra cerca del Cuzco, en el Perú. A primera vista pareciera una especie de anfiteatro, conformado de varios andenes circulares, situado a 3.500 msnm.</p>
+    </div>
+    <div class="caja">
+        <h3>3.El convento de Santa Catalina en la ciudad de Arequipa</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcJHGc.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f"height="270">
+        <p>El Monasterio de Santa Catalina de Siena, o Convento de Santa Catalina, es un complejo turístico religioso ubicado en el centro histórico de Arequipa, departamento de Arequipa, Perú.</p>
+    </div>
+    <div class="caja">
+        <h3>4. Centro arqueológico de Choquequirao en Cusco</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcJMEO.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" alt="LAGO TITICACA-Islas flotantes de los Uros" height="270">
+        <p>Ubicado a 3,104.09 msnm (Instituto Geográfico del Perú-2001) en lo alto de una verde montaña, en el límite de los departamentos de Cusco y Apurímac, el Parque Arqueológico de Choquequirao impresiona por la majestuosidad de la arquitectura Inca con andenes, plazas, recintos, grandes muros con hornacinas y otras construcciones.</p>
+    </div><div class="caja">
+        <h3>5.El valle del Colca en Arequipa</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcK0T0.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="270">
+        <p>El valle del Colca es uno de los mayores destinos turísticos del Perú. Está ubicado en la provincia de Caylloma. Colca proviene de las palabras Collaguas y Cabanas, dos etnias que habitaban a lo largo del río Colca. Este cañón tiene una profundidad de 4160 metros</p>
+    </div>
+    <div class="caja">
+        <h3>6.Las islas Ballestas en Paracas</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcK84i.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="150">
+        <p>Las islas Ballestas es un grupo de islas cerca de la ciudad de Pisco, en el Perú, están compuestas por formaciones rocosas donde se encuentra una importante fauna marina con aves guaneras como el guanay, el piquero y el zarcillo principalmente, destacan islas Ballestas Norte, Centro y Sur cada una con una superficie.. estimada en 0,12 km².U.</p>
+    </div>
+    <div class="caja">
+        <h3>7.Fortaleza de Kuélap</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcJYjA.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="270">
+        <p>Kuélap o Cuélap es un importante sitio arqueológico preinca ubicado en los Andes nororientales del Perú, en la Provincia de Luya, fue construido por la cultura arqueológica Chachapoyas.</p>
+    </div>
+    <div class="caja">
+        <h3>8.Las ventanillas de Otuzco en Cajamarca</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcJYjJ.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="270">
+        <p>Las Ventanillas de Otuzco, también llamadas Necrópolis de Otuzco, son unas edificaciones funerarias que por su forma parecen ser ventanas, de ahí su nominación de Ventanillas. Estas se encuentran ubicadas en el centro poblado de Otuzco, en el distrito de Cajamarca (a 8 kilómetros), en la provincia y departamento del mismo nombre</p>
+    </div>
+    <div class="caja">
+        <h3>9.Isla de Taquile en el Lago Titicaca en Puno</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcMekK.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="270">
+        <p>La isla de Taquile (Intika en quechua), en el lago Titicaca, pertenece al distrito de Amantaní, Puno, Perú y está situada a 45 km de la capital regional. Cuenta con una población aproximada de 2.200 habitantes. La villa principal se encuentra a 3.950 msnm y el punto más alto de la isla llega a los 4.050 msnm.</p>
+    </div>
+    <div class="caja">
+        <h3>10. Caral, la ciudad más antigua de América, en Lima</h3>
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAcMgRV.img?h=416&w=624&m=6&q=60&u=t&o=f&l=f" height="270">
+        <p>Caral es un sitio arqueológico donde se hallan los restos de la principal ciudad de la civilización Caral. Se encuentra en el valle de Supe, a 182 kilómetros al norte de Lima (Perú), a 23 km del litoral y a 350 msnm.</p>
+    </div>
+ 
 
 
 
